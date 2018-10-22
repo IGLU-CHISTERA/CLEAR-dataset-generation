799933a8bcc389bc12486696e0072d08bf18c628
J3romee <jerome.abdelnour@usherbrooke.ca>
2018-10-22 14:26:28 +0200

    + Added reverse tranformation for float_array to pydub object
    * Now using float64 during the translation to keep precision intact

--- utils/misc.py
+++ utils/misc.py
@@ -1,7 +1,11 @@
 import numpy as np
+from array import array
+from pydub import AudioSegment
+
+# TODO : Rename this file to data processing (Or something like that. Misc is too generic)
 
 
-def pydub_audiosegment_to_float_array(audio_segment, n_bytes):
+def pydub_audiosegment_to_float_array(audio_segment, frame_rate, n_bytes):
     """Convert an integer buffer to floating point values.
     This is primarily useful when loading integer-valued wav data
     into numpy arrays.
@@ -21,7 +25,19 @@ def pydub_audiosegment_to_float_array(audio_segment, n_bytes):
     fmt = '<i{:d}'.format(n_bytes)
 
     # Rescale and format the data buffer
-    return scale * np.frombuffer(raw_data, fmt).astype(np.float32)
+    return scale * np.frombuffer(raw_data, fmt).astype(np.float64)      # FIXME : Using float64 instead of float32 may cause memory/performance issues (Necessary to keep precision for reconversion)
+
+
+def float_array_to_pydub_audiosegment(float_array, frame_rate, n_bytes):
+    # Revert the scale of the data
+    scale = float(1 << ((8 * n_bytes) - 1))
+
+    array_of_samples = array('i', [int(scale * e) for e in float_array])
+
+    return AudioSegment(array_of_samples.tostring(),
+                        frame_rate=frame_rate,
+                        sample_width=n_bytes,
+                        channels=1)
 
 
 def midi_pitch_to_musical_note(self, midi_value):
