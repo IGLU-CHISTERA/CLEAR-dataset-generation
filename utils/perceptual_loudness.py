3957f148a2fc75f8b8ec766161bb9f8a7712e23f
J3romee <jerome.abdelnour@usherbrooke.ca>
2018-10-22 15:53:51 +0200

    * Modified perceptual loudness to fit the change in data conversion (utils/misc.py)

--- utils/perceptual_loudness.py
+++ utils/perceptual_loudness.py
@@ -6,4 +6,8 @@ def get_perceptual_loudness(pydub_audio_segment):
   # FIXME : The meter should probably not be created everytime
   loudness_meter = pyloudnorm.Meter(pydub_audio_segment.frame_rate, block_size=0.5)  # FIXME : Hardcoded block size
 
-  return loudness_meter.integrated_loudness(pydub_audiosegment_to_float_array(pydub_audio_segment, pydub_audio_segment.sample_width))
+  sound_float_array = pydub_audiosegment_to_float_array(pydub_audio_segment,
+                                                        pydub_audio_segment.frame_rate,
+                                                        pydub_audio_segment.sample_width)
+
+  return loudness_meter.integrated_loudness(sound_float_array)
