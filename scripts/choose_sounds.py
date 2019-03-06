ab6cdd1cfc466b93a893bf3c3058e1770b5ad784
J3romee <jerome.abdelnour@usherbrooke.ca>
2019-03-06 12:08:06 -0500

    * Good-sounds picker

--- scripts/choose_sounds.py
+++ scripts/choose_sounds.py
@@ -1,111 +1,65 @@
 import sqlite3
 import json
 import os
+from operator import attrgetter
 from shutil import copy2 as copyfile
 from shutil import rmtree as rmdir
+import argparse
+
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+
+from utils.audio_processing import get_perceptual_loudness
 
 
-
-
-
-
-
-def retrieve_cello_reference_sounds_from_db(cursor):
-  cursor.execute("SELECT sounds.instrument, sounds.note, sounds.octave, ratings.mark, takes.microphone, takes.filename from sounds \
-                  LEFT JOIN takes on takes.sound_id = sounds.id \
-                  LEFT JOIN packs on packs.id = sounds.pack_id \
-                  LEFT JOIN ratings on ratings.sound_id = sounds.id \
-                  WHERE sounds.octave = 3 \
-                  AND sounds.reference = 1 \
-                  AND sounds.instrument = 'cello'\
-                  AND sounds.klass = 'good-sound' \
-                  AND packs.name NOT LIKE '%recordings%' \
-                  AND takes.microphone != 'iphone' ")
-
-  return cursor.fetchall()
-
-
-def retrieve_reference_sounds_from_db_with_joins(cursor):
-  cursor.execute("SELECT sounds.instrument, sounds.note, sounds.octave, packs.name, ratings.mark, takes.microphone, takes.filename from sounds \
-                  LEFT JOIN takes on takes.sound_id = sounds.id \
-                  LEFT JOIN packs on packs.id = sounds.pack_id \
-                  LEFT JOIN ratings on ratings.sound_id = sounds.id \
-                  WHERE sounds.octave = 4 \
-                  AND (ratings.type = 'good-sound' OR ratings.type IS NULL) \
-                  AND takes.microphone != 'iphone' \
-                  AND packs.name LIKE '%reference' ")
-
-  return cursor.fetchall()
-
-def retrieve_all_sounds_from_db(cursor):
-  cursor.execute("SELECT sounds.instrument, sounds.note, sounds.octave, packs.name, ratings.mark, takes.microphone, takes.filename from sounds \
-                  LEFT JOIN takes on takes.sound_id = sounds.id \
-                  LEFT JOIN packs on packs.id = sounds.pack_id \
-                  LEFT JOIN ratings on ratings.sound_id = sounds.id \
-                  WHERE sounds.klass = 'good-sound' \
-                  AND takes.microphone = 'neumann' ")
-
-  return cursor.fetchall()
-
-
-
-
-
-
-    
-#################################################
-#### NOT REALLY USEFUL
-def retrieve_reference_sounds_all_octaves_from_db(cursor):
-  cursor.execute("SELECT sounds.instrument, sounds.note, sounds.octave, ratings.mark, takes.microphone, takes.filename from sounds \
-                  LEFT JOIN takes on takes.sound_id = sounds.id \
-                  LEFT JOIN packs on packs.id = sounds.pack_id \
-                  LEFT JOIN ratings on ratings.sound_id = sounds.id \
-                  WHERE sounds.reference = 1 \
-                  AND sounds.klass = 'good-sound' \
-                  AND packs.name NOT LIKE '%recordings%' \
-                  AND takes.microphone != 'iphone' ")
-
-  return cursor.fetchall()
-
-
-def get_note_availability(all_sounds):
-  notes_per_octave_per_instrument = {}
-  note_count_per_octave_per_instrument = {}
-
-  for sound in all_sounds:
-    if sound['instrument'] not in notes_per_octave_per_instrument:
-      notes_per_octave_per_instrument[sound['instrument']] = {}
-
-    if sound['octave'] not in notes_per_octave_per_instrument[sound['instrument']]:
-      notes_per_octave_per_instrument[sound['instrument']][sound['octave']] = []
-
-    notes_per_octave_per_instrument[sound['instrument']][sound['octave']].append(sound['human_note'])
-
-    notes_per_octave_per_instrument[sound['instrument']][sound['octave']] = sorted(list(set(notes_per_octave_per_instrument[sound['instrument']][sound['octave']])))
-
-    for instrument in notes_per_octave_per_instrument:
-      if instrument not in note_count_per_octave_per_instrument:
-        note_count_per_octave_per_instrument[instrument] = {}
-
-      for octave in notes_per_octave_per_instrument[instrument]:
-        note_count_per_octave_per_instrument[instrument][octave] = len(notes_per_octave_per_instrument[instrument][octave])
-
-  return notes_per_octave_per_instrument, note_count_per_octave_per_instrument
-
-
-
-################################################
-#### STUFF THAT IS USED
-################################################
 '''
-SQL Handling
+Arguments definition
 '''
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--good_sounds_folder', type=str, default="./good-sounds/sounds_files",
+                    help='Path to the good-sounds audio folder')
+
+parser.add_argument('--good_sounds_database_filename', type=str, default="./good-sounds/database.sqlite",
+                    help='Path to the good-sounds sqlite database')
+
+parser.add_argument('--output_path', type=str, default="./elementary_sounds",
+                    help='Path were the extracted elementary sounds will be copied')
+
+parser.add_argument('--output_definition_filename', type=str, default="elementary_sounds.json",
+                    help='Filename for the json file that store the attributes of the elementary sounds')
+
+
+# Preprocessing parameters
+# FIXME : Write the help messages
+parser.add_argument('--silence_threshold', type=float, default=-50,
+                    help='')
+
+parser.add_argument('--min_silence_duration', type=int, default=100,
+                    help='')
+
+parser.add_argument('--amplification_factor', type=float, default=-10,
+                    help='')
+
+parser.add_argument('--amplification_low_bound', type=float, default=-30.5,
+                    help='')
+
+parser.add_argument('--amplification_high_bound', type=float, default=-24.0,
+                    help='')
+
+
 def connect_db(database_path):
+  """
+  SQL Database connection
+  """
   connection = sqlite3.connect(database_path)
   return connection.cursor()
 
 
-def retrieve_CLEAR_elementary_sounds_from_goodsounds_db(database):
+def retrieve_CLEAR_elementary_sounds_infos_from_goodsounds_db(database):
+  """
+  SQL Query
+  """
   database.execute("SELECT sounds.instrument, sounds.note, sounds.octave, takes.filename from sounds \
                     LEFT JOIN takes on takes.sound_id = sounds.id \
                     LEFT JOIN packs on packs.id = sounds.pack_id \
@@ -115,13 +69,7 @@ def retrieve_CLEAR_elementary_sounds_from_goodsounds_db(database):
                     AND packs.name NOT LIKE '%recordings%' \
                     AND takes.microphone = 'akg' ")
 
-  return database.fetchall()
-
-
-soundsColumns = ['id', 'instrument', 'note', 'octave', 'dynamics', 'recorded_at', 'location',
-                'player', 'bow_velocity', 'bridge_position', 'string', 'csv_file', 'csv_id',
-                'pack_filename', 'pack_id', 'attack', 'decay', 'sustain', 'release', 'offset',
-                'reference', 'klass', 'comments', 'semitone', 'pitch_reference']
+  return db_rows_to_dict(database.fetchall())
 
 
 def db_rows_to_dict(results):
@@ -141,110 +89,99 @@ def db_rows_to_dict(results):
   return sounds
 
 
-'''
-Filtering
-'''
-
-
-def overwrite_folder(folder_path):
-  if os.path.isdir(folder_path):
-    input(">>> The folder '%s' already exist. \n>>> Press enter to overwrite it. Otherwise CTRL+C" % folder_path)
-    rmdir(folder_path)
-
-  os.mkdir(folder_path)
-
-
-def create_folder_if_doesnt_exist(folder_path):
-  if not os.path.isdir(folder_path):
-    os.mkdir(folder_path)
-
-
-def copy_sounds_file(sounds, dataset_root_path, destination_path, definition_filename):
-  overwrite_folder(destination_path)
-
-  print("Starting to copy files in '%s'" % destination_path)
+def load_sounds_in_memory(sounds, good_sounds_folder):
+  """
+  Load audio segments into memory
+  """
   for sound in sounds:
-    new_filename = '%s_%s_%s.wav' % (sound['instrument'], sound['human_note'], sound['source_microphone'])
+    audio_segment = AudioSegment.from_wav(os.path.join(good_sounds_folder, sound['filename']))
+    sounds['audio_segment'] = audio_segment
 
-    copyfile(os.path.join(dataset_root_path, sound['filename']), os.path.join(destination_path, new_filename))
+  return sounds
 
+
+def write_sounds_to_files(sounds, output_path, definition_filename):
+  """
+  Write the audio segments to file and export the definition JSON file
+  """
+
+  if os.path.isdir(output_path):
+    input(">>> The folder '%s' already exist. \n>>> Press enter to overwrite it. Otherwise CTRL+C" % output_path)
+    rmdir(output_path)
+
+  os.mkdir(output_path)
+
+  for sound in sounds:
+    new_filename = '%s_%s.wav' % (sound['instrument'], sound['note'])
+    sound['audio_segment'].export(os.path.join(output_path, new_filename))
     sound['filename'] = new_filename
+    del sound['audio_segment']
 
-  with open(os.path.join(destination_path, definition_filename), 'w') as f:
+  with open(os.path.join(output_path, definition_filename), 'w') as f:
     json.dump(sounds, f, indent=2)
 
-  print("Finished copying files")
+  print("Elementary sounds successfully written in '%s'" % output_path)
 
 
-def copy_sounds_file_separate_microphone(sounds, dataset_root_path, destination_path, definition_filename):
-  overwrite_folder(destination_path)
-  definitions_by_microphone = {}
-
-  print("Starting to copy files in '%s'" % destination_path)
+def remove_silence(sounds, silence_thresh, min_silence_duration):
+  """
+  Remove the silence parts of the recordings
+  The recordings that we are filtering are sustained note instrumental recordings
+  therefore we keep only the longest non silent interval
+  """
   for sound in sounds:
-    new_filename = '%s_%s_%s.wav' % (sound['instrument'], sound['human_note'], sound['source_microphone'])
-    microphone_folder = os.path.join(destination_path, sound['source_microphone'])
-    create_folder_if_doesnt_exist(microphone_folder)
+    non_silent_audio_chunks = split_on_silence(sound['audio_segment'],
+                                               min_silence_duration,
+                                               silence_thresh,
+                                               100)
 
-    copyfile(os.path.join(dataset_root_path, sound['filename']), os.path.join(microphone_folder, new_filename))
+    # Sort the non silent part by duration
+    non_silent_audio_chunks = sorted(non_silent_audio_chunks, key=attrgetter('duration_seconds'), reverse=True)
 
-    sound['filename'] = new_filename
+    # Use the longest non-silent part
+    # (This is suitable only for the recordings of Good-Sounds dataset since they are sustained instrumental notes)
+    sound['audio_segment'] = non_silent_audio_chunks[0]
 
-    if sound['source_microphone'] not in definitions_by_microphone:
-      definitions_by_microphone[sound['source_microphone']] = []
-
-    definitions_by_microphone[sound['source_microphone']].append(sound)
-
-  for microphone, sounds_def in definitions_by_microphone.items():
-    definition_path = os.path.join(destination_path, microphone, definition_filename)
-
-    with open(definition_path, 'w') as f:
-      json.dump(sounds_def, f, indent=2)
-
-  print("Finished copying files")
+  return sounds
 
 
-def copy_sounds_file_separate_folders_per_instrument(sounds, dataset_root_path, destination_path, definition_filename):
-  overwrite_folder(destination_path)
-
-  print("Starting to copy files in '%s'" % destination_path)
+def amplify_if_perceptual_loudness_in_range(sounds, amplification_factor, low_bound, high_bound):
+  """
+  If the perceptual loudness of the sound is between low_bound and high_bound --> Amplify it by the amplification_factor
+  """
   for sound in sounds:
-    new_filename = '%s_%s.wav' % (sound['instrument'], sound['human_note'])
+    perceptual_loudness = get_perceptual_loudness(sound['audio_segment'])
 
-    instrument_folder = os.path.join(destination_path, sound['instrument'])
-    create_folder_if_doesnt_exist(instrument_folder)
+    if high_bound > perceptual_loudness > low_bound:
+      sounds['audio_segment'] = sound['audio_segment'].apply_gain(amplification_factor)
 
-    source_folder = os.path.join(instrument_folder, sound['source_microphone'])
-    create_folder_if_doesnt_exist(source_folder)
-
-    copyfile(os.path.join(dataset_root_path, sound['filename']), os.path.join(source_folder, new_filename))
-
-  print("Finished copying files")
+  return sounds
 
 
-def main():
-  good_sounds_dataset_root_path = '.'
-  output_path = 'filtered'
-  good_sounds_db_path = 'database.sqlite'
-  database = connect_db(good_sounds_db_path)
+def main(args):
+  database = connect_db(os.path.join(args.good_sounds_folder, args.good_sounds_database_filename))
 
-  allSounds = retrieve_reference_sounds_all_octaves_from_db(cursor)
-  allSounds = db_rows_to_dict(allSounds)
+  # Loading sounds
+  CLEAR_elementary_sounds_infos = retrieve_CLEAR_elementary_sounds_infos_from_goodsounds_db(database)
+  CLEAR_elementary_sounds = load_sounds_in_memory(CLEAR_elementary_sounds_infos, args.good_sounds_folder)
 
-  notes_availability, note_count = get_note_availability(allSounds)
+  # Preprocessing
+  # Remove silence parts
+  CLEAR_elementary_sounds_preprocessed = remove_silence(CLEAR_elementary_sounds,
+                                                        args.silence_threshold,
+                                                        args.min_silence_duration)
 
-  print("Note count per instrument per octave")
-  print(json.dumps(notes_availability, indent=2))
+  # Amplify sounds that are in a certain range of perceptual loudness
+  CLEAR_elementary_sounds_preprocessed = amplify_if_perceptual_loudness_in_range(CLEAR_elementary_sounds_preprocessed,
+                                                                                 args.amplification_factor,
+                                                                                 args.amplification_low_bound,
+                                                                                 args.amplification_high_bound)
 
-  sounds = retrieve_reference_sounds_fourth_octave_from_db(cursor)
-  sounds = db_rows_to_dict(sounds)
+  # Write to file
+  write_sounds_to_files(CLEAR_elementary_sounds_preprocessed, args.output_path, args.output_definition_filename)
 
-  #copy_sounds_file_separate_folders_per_instrument(sounds, good_sounds_dataset_root_path, 'filtered_separated_per_instrument', 'elementary_sounds.json')
-
-  #copy_sounds_file_separate_microphone(sounds, good_sounds_dataset_root_path, output_path, 'elementary_sounds.json')
-
-  print("All done")
 
 if __name__ == "__main__":
-  main()
+  args = parser.parse_args()
+  main(args)
 
